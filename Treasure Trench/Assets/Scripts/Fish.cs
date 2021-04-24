b94ef14a95363df89bd8bf654441a123ef4a064e
Sharven Prasad Dhanasekar <sharven.dhanasekar@mail.utoronto.ca>
2021-04-24 17:52:13 -0400

    Add fish and dash fish

--- Treasure Trench/Assets/Scripts/Fish.cs
+++ Treasure Trench/Assets/Scripts/Fish.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Fish : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public int damage;
+	public float minX;
+	public float maxX;
+	public Vector2 moveSpeed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	protected Rigidbody2D rb;
+	protected SpriteRenderer sr;
+	protected float directionMultiplier;
+	protected float startY;
+	protected float endY;
+
+	protected void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		sr = GetComponent<SpriteRenderer>();
+
+		startY = -Camera.main.orthographicSize - 1f;
+		endY = Camera.main.orthographicSize + 1f;
+
+		transform.position = new Vector2(Random.Range(minX, maxX), startY);
+		directionMultiplier = Random.value > 0.5f ? 1 : -1;
+		sr.flipX = directionMultiplier != 1;
+	}
+
+	protected void FixedUpdate()
+	{
+		// Move the fish up and left/right
+		if (rb.position.x < minX && directionMultiplier == -1)
+		{
+			directionMultiplier = 1;
+		}
+		else if (rb.position.x > maxX && directionMultiplier == 1)
+		{
+			directionMultiplier = -1;
+		}
+		
+		sr.flipX = directionMultiplier != 1;
+
+		//rb.MovePosition(rb.position + Vector2.Scale(new Vector2(directionMultiplier, 1f), moveSpeed) * Time.deltaTime);
+		rb.velocity = new Vector2(directionMultiplier * moveSpeed.x, moveSpeed.y) * Time.deltaTime;
+
+		// Despawn this fish
+		if (rb.position.y > endY)
+			gameObject.SetActive(false);
+	}
 }
