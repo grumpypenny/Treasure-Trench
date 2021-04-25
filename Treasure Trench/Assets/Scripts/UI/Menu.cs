f557ea291eaa1267f7ffaf1af47d455578801ff9
Sharven Prasad Dhanasekar <sharven.dhanasekar@mail.utoronto.ca>
2021-04-25 14:08:15 -0400

    Add folders in the scripts folder

--- Treasure Trench/Assets/Scripts/UI/Menu.cs
+++ Treasure Trench/Assets/Scripts/UI/Menu.cs
@@ -14,6 +14,7 @@ public class Menu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+		Time.timeScale = 1f;
 		anim = GetComponent<Animator>();
     }
 
