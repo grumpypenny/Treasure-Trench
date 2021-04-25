c9c9ce325853f4a51a26dd972098457295584a41
grumpypenny <ajitesh.misra.14@gmail.com>
2021-04-25 18:26:09 -0400

    add lighting system and death

--- Treasure Trench/Assets/Scripts/Player/PlayerHealth.cs
+++ Treasure Trench/Assets/Scripts/Player/PlayerHealth.cs
@@ -11,6 +11,7 @@ public class PlayerHealth : MonoBehaviour, IHealth
 
 	private Animator anim;
 	private Light2D light2d;
+	private Rigidbody2D rb;
 	private float startRadius;
 
 	// Start is called before the first frame update
@@ -20,7 +21,8 @@ public class PlayerHealth : MonoBehaviour, IHealth
 		anim = GetComponent<Animator>();
 		light2d = GetComponentInChildren<Light2D>();
 		startRadius = light2d.pointLightInnerRadius;
-    }
+		rb = GetComponent<Rigidbody2D>();
+	}
 
 	void IHealth.DealDamage(int damage)
 	{
@@ -35,12 +37,22 @@ public class PlayerHealth : MonoBehaviour, IHealth
 
 	private void GetHit()
 	{
+		anim.SetTrigger("Hit");
 		// make the light smaller
 		light2d.pointLightInnerRadius -= startRadius / maxHealth;
 	}
 
 	private void Die()
 	{
+		StartCoroutine(delay());
+		anim.SetTrigger("Die");
+		rb.gravityScale = 2;
+		GetComponent<PlayerMovement>().enabled = false;
+	}
+
+	IEnumerator delay()
+	{
+		yield return new WaitForSeconds(5);
 		gameObject.SetActive(false);
 	}
 }
